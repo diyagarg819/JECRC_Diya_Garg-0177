f8028989757fc9912d93d5fe1a41bcc482e9c239
Diya Garg <garg12064@gmail.com>
2026-04-10 17:06:21 +0530

    Assessment on webapi

---
+++ Week-8/10-08-2026/Assessments/WebApplication1/WebApplication1/dto/BookResponsedto.cs
@@ -0,0 +1,23 @@
+﻿namespace LibraryManagement.dto
+{
+    public class BookResponsedto
+    {
+
+            public required string BookName { get; set; } 
+
+            public required string Author { get; set; }
+
+            public required string ISBN { get; set; } 
+
+            public required string Genre { get; set; } 
+
+            public DateTime? PublishedDate { get; set; }
+
+            public int AvailableCopies { get; set; }
+
+            public string? Publisher { get; set; }
+
+            public string? Language { get; set; } 
+        
+    }
+}
---
+++ Week-8/10-08-2026/Assessments/WebApplication1/WebApplication1/entities/Book.cs
@@ -0,0 +1,25 @@
+﻿namespace LibraryManagement.Models
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+
+        public string BookName { get; set; } = string.Empty;
+
+        public string Author { get; set; } = string.Empty;
+
+        public string ISBN { get; set; } = string.Empty;
+
+        public string Genre { get; set; } = string.Empty;
+
+        public DateTime? PublishedDate { get; set; }
+
+        public int TotalCopies { get; set; }
+
+        public int AvailableCopies { get; set; }
+
+        public string? Publisher { get; set; }
+
+        public string? Language { get; set; }
+    }
+}
